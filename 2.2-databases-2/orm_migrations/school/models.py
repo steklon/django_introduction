9a64cdde33bac0851ec6e94a8b5c888ae54533c1
steklone <steklo_one@mail.ru>
2024-03-19 01:35:17 +1000

    orm_migrations task completed

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Teacher(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
 
@@ -14,8 +15,9 @@ class Teacher(models.Model):
 
 
 class Student(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher, related_name='students')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
