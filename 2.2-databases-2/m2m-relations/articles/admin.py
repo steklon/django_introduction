cbbf629a356ab31dbbb543a5cf88f5e90d46d4c9
steklone <steklo_one@mail.ru>
2024-03-21 22:52:35 +1000

    homework completedm2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main_counter = 0
+        tag_list = list()
+        for form in self.forms:
+            try:
+                if form.cleaned_data:
+                    tag_list.append(form.cleaned_data['tag'])
+                    if form.cleaned_data['is_main']:
+                        is_main_counter += 1
+            except KeyError:
+                raise ValidationError('Для удаления раздела из статьи используйте "УДАЛИТЬ"')
+
+            if len(tag_list) != len(set(tag_list)):
+                raise ValidationError('В статье дублируются разделы')
+            if is_main_counter > 1:
+                raise ValidationError('Основной раздел может быть только один')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 3
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,28 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+
+    news = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематики статьи'
+        verbose_name_plural = 'Тематики'
+        ordering = ['-is_main']
