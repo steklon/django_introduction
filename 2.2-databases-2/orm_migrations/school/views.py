cdbd86542929fcb0c3c70a6f602d37d45e44db49
steklone <steklo_one@mail.ru>
2024-03-21 23:09:54 +1000

    fixed bug in views.py

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -10,7 +10,6 @@ def students_list(request):
     students = Student.objects.all().order_by(ordering)
     context = {'object_list': students}
 
-
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
 
