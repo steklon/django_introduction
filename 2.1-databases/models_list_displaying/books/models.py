345ae4a99414be500066c22223f6f8636670655c
steklone <steklo_one@mail.ru>
2024-03-21 23:00:52 +1000

    added solution part 2.1-databases

--- 2.1-databases/models_list_displaying/books/models.py
+++ 2.1-databases/models_list_displaying/books/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 
 class Book(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(u'Название', max_length=64)
     author = models.CharField(u'Автор', max_length=64)
     pub_date = models.DateField(u'Дата публикации')
 
-    def __str__(self):
-        return self.name + " " + self.author
+    # def __str__(self):
+    #     return self.name + " " + self.author
--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,50 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from books.converters import DateConverter
+from books.models import Book
+
+
+def books_home(request):
+    template = 'books/books_list.html'
+    context = {}
+    return render(request, template, context)
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books_list = Book.objects.all()
+    date_object = DateConverter()
+    books_date_list = [{"date": date_object.to_url(b.pub_date),
+                        "name": b.name,
+                        'author': b.author
+                        } for b in books_list]
+    # books_date_list = [date_con.to_url(b.pub_date) for b in books_list]
+    print(books_date_list)
+
+    path = request.path[1:-1]
+    print(path)
+    context = {'books': books_date_list}
+    return render(request, template, context)
+
+
+def books_date_view(request, pub_date):
+    template = 'books/books_list_date.html'
+    date_object = DateConverter()
+    books_list = Book.objects.filter(pub_date=date_object.to_python(pub_date))
+    # book_back = Book.objects.filter(pub_date__lt=date_object.to_python(pub_date)).values('pub_date').first()
+    # print(book_back)
+    # # books_date_list = [{"date": date_object.to_url(b.pub_date),
+    # #                     "name": b.name,
+    # #                     'author': b.author
+    # #                     } for b in books_list]
+    #
+    paginator = Paginator(books_list, 1)
+    page = paginator.get_page(pub_date)
+    # print(page)
+    context = {'books': books_list,
+               'page': page
+               }
+
     return render(request, template, context)
